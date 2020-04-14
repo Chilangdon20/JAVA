5c0bbeac509acf2ee4da1cded55852210e02fa62
Fernando_Vazquez <61173776+Chilangdon20@users.noreply.github.com>
2020-04-14 10:16:15 -0500

    Add files via upload

---
+++ Conceptos Intermedios/Encapsulacion/ModificadoresAcesso/ModificadoresAcceso/ModificadorPrivate/src/paquete1/ClaseJavaUno.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete1;
+
+/**
+ *
+ * @author FAMILIA
+ */
+public class ClaseJavaUno {
+    public static void main(String[] args){
+        //declaracion de objeto
+        Atributos_Metodos mensajero = new Atributos_Metodos();
+        int valorDoos = mensajero.valorDos;
+        
+    }
+    
+}
