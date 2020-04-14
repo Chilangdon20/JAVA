3038d3822762a27c917d567f1065a76a5d68e428
Fernando_Vazquez <61173776+Chilangdon20@users.noreply.github.com>
2020-04-14 10:25:41 -0500

    Add files via upload

---
+++ Conceptos Intermedios/Encapsulacion/ModificadoresAcesso/ModificadoresAcceso/ModificadorProtected/src/paquete2/ClaseJavaDos.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete2;
+import paquete1.Atributos_Metodos;
+/**
+ *
+ * @author F.V
+ */
+public class ClaseJavaDos {
+    public static void main(String[] args){
+        
+    
+    Atributos_Metodos mensajero = new Atributos_Metodos();
+    mensajero.valorTres;
+    
+    }
+}
