faead4f3700df618f9cecf7c6baf4361e0f9d038
Fernando_Vazquez <61173776+Chilangdon20@users.noreply.github.com>
2020-10-18 12:18:49 -0500

    Update Ejercicio04.java

--- Conceptos Básicos/Ejercicios_Progra_1/EJERCICIOS_BASICOS/Ejercicio04.java
+++ Conceptos Básicos/Ejercicios_Progra_1/EJERCICIOS_BASICOS/Ejercicio04.java
@@ -1,37 +1,59 @@
-// package ejercicios;
+package ejercicios;
 
 //Importo todos los elementos de la biblioteca time , esto para poder jugar con las fechas.
 
+
 import java.time.*; 
-
-//Biblioteca para pedir datos al usuario
-import java.util.Scanner;
-
-public class Main
+import java.util.Scanner; 
+import java.util.GregorianCalendar;
+import java.util.Calendar;
+public class Cumple
 {
 	public static void main(String[] args) {
+		
+		
+		GregorianCalendar tiempo = new GregorianCalendar();
+		int year = tiempo.get(Calendar.YEAR);
+		int mes = tiempo.get(Calendar.DAY_OF_MONTH);
+		int dia = tiempo.get(Calendar.DAY_OF_WEEK);
+		
+		
+        // Verificamos que cada componente de la fecha sea de tipo entero.		
+		if (year == (int)year & mes == (int)mes & dia == (int)dia) {
+			
+			
+				
+				// Entrada del usuario: aaaa-mm-dd
+			    Scanner scnr = new Scanner(System.in);
+			    System.out.println("Hola :D , ingresa una fecha en formato aaaa-mm-dd: "); 
+			    // Almacenamos la variable como String
+			    String dob = scnr.next(); 
+			    
+			    // Convertimos de tipo string a fecha 
+				LocalDate FechaCumpleaños = LocalDate.parse(dob);
+				
+				
+				// Obtenemos la fecha actual para el calculo de la edad.
+				LocalDate currDate = LocalDate.now(); 
+				
+				
+				// Obtenemos la diferencia entre las 2 fechas
+				Period period = Period.between(currDate, FechaCumpleaños);
+				
+				
+				// Aíslamos la cantidad de años para obtener el formato de edad adecuado
+				int age  = Math.abs(period.getYears()); 
+		     
+		        System.out.println("La fecha  que ingresaste es: " + dob + " al día de hoy tienes: " + age + " años  adelante , la vida sigue."); 
+				
+			
+			
+		}
+		else {
+			System.out.println("Los datos ingresados no son de tipo entero, porfavor ingresa datos validos.");
+		}
+		      
 	    
-	    // Entrada del usuario: aaaa-mm-dd
-	    Scanner scnr = new Scanner(System.in);
-	    System.out.println("Hola :D , ingresa una fecha en formato aaaa-mm-dd: "); // Print original input
-	    // Almacenamos la variable como String
-	    String data = scnr.next(); 
 	    
-	    // Convertimos de tipo string a fecha 
-		LocalDate FechaCumpleaños = LocalDate.parse(data);
-		
-		
-		// Obtenemos la fecha actual para el calculo de la edad.
-		LocalDate fechaActual = LocalDate.now(); 
-		
-		
-		// Obtenemos la diferencia entre las 2 fechas
-		Period dife = Period.between(fechaActual, FechaCumpleaños);
-		
-		
-		// Aíslamos la cantidad de años para obtener el formato de edad adecuado
-		int edad  = Math.abs(dife.getYears()); 
-     
-        System.out.println("La fecha  que ingresaste es: " + data + " al día de hoy tienes: " + edad + " años  adelante , la vida sigue."); 
 	}
 }
